97ebc79161c8c754e0757f8d4b6ddf77756dc396
Biki-das <bikid475@gmail.com>
2022-11-27 12:48:43 +0530

    Desktop design commit

---
+++ main.js
@@ -0,0 +1,110 @@
+import "./style.css";
+
+const cartDetails = document.querySelector("#cart-details");
+const cartTab = document.querySelector("#cart-tab");
+const cartInfo = document.querySelector("#cart-info");
+const ImageHolder = document.querySelectorAll("#img-gallery");
+const current = document.querySelector("#active-image");
+const opacity = "0.4";
+const cartCount = document.querySelector("#cart-count");
+const cartIncreasebtn = document.querySelector("#btn-decrease");
+const cartDecreasebtn = document.querySelector("#btn-increase");
+const Add_card_btn = document.querySelector("#Add-cart-btn");
+const Cart_item_count = document.querySelector("#cart-item-count");
+const singlePrice = document.querySelector("#item-price");
+const totalQty = document.querySelector("#total-quantity");
+const totalPrice = document.querySelector("#total-price");
+const deleteCart = document.querySelector("#delete-cart");
+const checkoutBtn = document.querySelector("#checkout-btn");
+const cartEmpty = document.querySelector("#cart-empty");
+
+// cart-details toggler
+
+cartTab.addEventListener("mouseover", () => {
+  cartDetails.classList.remove("hidden");
+});
+
+cartDetails.addEventListener("mouseleave", () => {
+  cartDetails.classList.add("hidden");
+});
+
+// ImageGallery
+
+ImageHolder.forEach((img) =>
+  img.addEventListener("click", (e) => {
+    ImageHolder.forEach((img) => (img.style.opacity = 1));
+    ImageHolder.forEach((img) => (img.style.border = "none"));
+    current.src = e.target.src;
+    e.target.style.opacity = opacity;
+    e.target.style.border = "2px solid orange";
+  })
+);
+
+// Cart-Holder-logic
+
+cartIncreasebtn.addEventListener("click", () => {
+  let count = cartCount.textContent;
+  count++;
+  cartCount.textContent = count;
+});
+
+cartDecreasebtn.addEventListener("click", () => {
+  let count = cartCount.textContent;
+  if (count > 0) {
+    count--;
+    cartCount.textContent = count;
+  }
+});
+
+// cart-item-add logic
+
+Add_card_btn.addEventListener("click", () => {
+  let count = cartCount.textContent;
+  Cart_item_count.textContent = count;
+
+  //
+
+  if (count > 0) {
+    console.log(cartEmpty);
+    checkoutBtn.classList.remove("hidden");
+    checkoutBtn.classList.add("flex");
+    cartInfo.classList.remove("hidden");
+    cartInfo.classList.add("flex");
+    cartEmpty.classList.add("hidden");
+  }
+
+  // setting the cart amount
+  totalQty.textContent = count;
+  let unitPrice = singlePrice.textContent.split("$")[1];
+  let finalQuantity = totalQty.textContent;
+  let finalPrice = unitPrice * finalQuantity;
+  totalPrice.textContent = "$" + `${finalPrice}` + ".00";
+});
+
+// cart-delete-logic
+
+deleteCart.addEventListener("click", () => {
+  let count = 0;
+  checkoutBtn.classList.add("hidden");
+  cartInfo.classList.add("hidden");
+  cartEmpty.classList.remove("hidden");
+  Cart_item_count.textContent = count;
+});
+
+//checkout-logic
+
+checkoutBtn.addEventListener("click", () => {
+  Swal.fire(
+    "Congratulations",
+    "Your order has been placed successfully!",
+    "success"
+  );
+  let count = 0;
+  checkoutBtn.classList.add("hidden");
+  cartInfo.classList.add("hidden");
+  cartEmpty.classList.remove("hidden");
+  cartCount.textContent = count;
+  Cart_item_count.textContent = count;
+  totalQty.textContent = count;
+  totalPrice.textContent = "$" + `${count}` + ".00";
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        Orange: "hsl(26, 100%, 55%)",
+        Paleorange: "hsl(25, 100%, 94%)",
+        veryDarkBlue: "hsl(220, 13%, 13%)",
+        Darkgrayishblue: "hsl(219, 9%, 45%)",
+        Grayishblue: "hsl(220, 14%, 75%)",
+        Lightgrayishblue: "hsl(223, 64%, 98%)",
+        White: "hsl(0, 0%, 100%)",
+        Black: "#7a7a7c",
+        LightOrange: "#ffac6a",
+      },
+    },
+  },
+  plugins: [],
+};
